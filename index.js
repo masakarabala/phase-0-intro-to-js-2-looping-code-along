1cebcc2839dd03b85400d8d1a26a79419bed854d
masakarabala <48476935+masakarabala@users.noreply.github.com>
2023-04-30 11:47:35 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, birthday) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+  }
+  console.log(writeCards(names));
+
+
+function countDown(){
+   let x = 10;
+while (x >=0) {
+    console.log(x)
+    x--; // x = x - 1
+ 
+}
+}
+countDown()
+  
+
+  
+
+
